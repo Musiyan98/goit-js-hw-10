6047c004394378e836b57bf1bf417726c601e7c7
Musiyan98 <musiyan98@gmail.com>
2022-09-28 18:20:34 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let intervalId = null;
+
+startButtonEl.addEventListener('click', onStartColorChange);
+stopButtonEl.addEventListener('click', onStopColorChange);
+
+function onStartColorChange() {
+  startButtonEl.setAttribute('disabled', '');
+  changeColor();
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function onStopColorChange() {
+  startButtonEl.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+
+function changeColor() {
+  let bgColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = bgColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
