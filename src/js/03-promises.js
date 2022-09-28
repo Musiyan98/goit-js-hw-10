5f0438991ef60895a6a91bc384a7e131b77b577b
Musiyan98 <musiyan98@gmail.com>
2022-09-28 23:38:51 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+const formEl = document.querySelector('.form');
+const delayInputEl = document.querySelector('input[name="delay"]');
+const stepInputEl = document.querySelector('input[name="step"]');
+const amountInputEl = document.querySelector('input[name="amount"]');
+
+let amountOfPromise;
+let delayThisStep;
+
+formEl.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
